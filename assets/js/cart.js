6b1c99d06312237e1543d99fbc1e4740bb08fce8
samavm <88936498+samavm@users.noreply.github.com>
2023-01-12 19:39:30 +0400

    fifth commit

---
+++ assets/js/cart.js
@@ -0,0 +1,36 @@
+let row = document.querySelector(".row");
+if (
+  localStorage.getItem("basket") === null ||
+  localStorage.getItem("basket") == "[]"
+) {
+  localStorage.setItem("basket", JSON.stringify([]));
+  let div = document.createElement("div");
+  let p = document.createElement("p");
+
+  p.innerText = "Your bag is empty";
+  p.style.fontSize = "30px";
+  div.appendChild(p);
+  row.append(div);
+} else {
+  let basket = JSON.parse(localStorage.getItem("basket"));
+
+  basket.forEach((p) => {
+    let div = document.createElement("div");
+    let image = document.createElement("img");
+    let pCount = document.createElement("p");
+    let pPrice = document.createElement("p");
+
+    div.className = "col-lg-3";
+
+    image.style.width = "150px";
+    image.src = p.image;
+
+    pCount.innerText = "Count: " + p.count;
+
+    pPrice.innerText = "Price: " + p.price + "$";
+
+    div.append(image, pCount, pPrice);
+
+    row.append(div);
+  });
+}
